d02559fa7ee73c69d81117448437b3f39b03eecf
KaioDanielLC <kaiodaniellc@gmail.com>
2024-08-09 23:17:23 -0300

    Botão de mostrar senha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,10 +16,19 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <div class="relative">
+                <x-text-input id="password" class="block mt-1 w-full"
+                    type="password"
+                    name="password"
+                    required autocomplete="current-password" />
+
+                <!-- Botão para mostrar/ocultar senha -->
+                <button type="button" onclick="togglePassword()" class="absolute inset-y-0 right-0 pr-3 flex items-center">
+                    <svg id="password-icon" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 4a6 6 0 00-6 6 6 6 0 0012 0 6 6 0 00-6-6zm0 2a4 4 0 000 8 4 4 0 000-8zM2.458 10C3.732 7.943 6.448 6 10 6s6.268 1.943 7.542 4c-1.274 2.057-3.99 4-7.542 4s-6.268-1.943-7.542-4z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </div>
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -34,9 +43,9 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
             @endif
 
             <x-primary-button class="ms-3">
