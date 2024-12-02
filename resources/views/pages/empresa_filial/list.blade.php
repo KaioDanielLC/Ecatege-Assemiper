cd22a3c13c18e75d86e2722fcc3addc848e7bc65
GabrielCosta000 <gabcosgon@gmail.com>
2024-12-02 09:12:52 -0300

    Ajuste na fonte da listagem das Empresas Filiais

--- resources/views/pages/empresa_filial/list.blade.php
+++ resources/views/pages/empresa_filial/list.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout> @if (session()->has('message'))
-    <div id="alert-border-1" class="flex items-center p-4 mb-4 text-blue-100 border-blue-700 bg-blue-400" role="alert">
+    <div id="alert-border-1" class="flex items-center p-4 mb-4 text-blue-100 border-blue-700 bg-blue-700" role="alert">
         <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
         </svg>
         <div class="ms-3 text-sm font-medium">
             {{session()->get('message')}}
         </div>
-        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 " data-dismiss-target="#alert-border-1" aria-label="Close">
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-700 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 " data-dismiss-target="#alert-border-1" aria-label="Close">
             <span class="sr-only">Dismiss</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
@@ -43,31 +43,31 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center col-span-2">{{$empresa_filiais->nome_empresa}}</h5>
                     <div>
                         <p>Nome do Titular:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">{{$empresa_filiais->nome_dono}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700">{{$empresa_filiais->nome_dono}}</p>
                     </div>
                     <div>
                         <p>Endereço:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">{{$empresa_filiais->endereco}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700">{{$empresa_filiais->endereco}}</p>
                     </div>
                     <div>
                         <p>Celular:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">{{$empresa_filiais->celular}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700">{{$empresa_filiais->celular}}</p>
                     </div>
                     <div>
                         <p>WhatsApp:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">{{$empresa_filiais->whatsapp}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700">{{$empresa_filiais->whatsapp}}</p>
                     </div>
                     <div>
                         <p>Telefone Fixo:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">{{$empresa_filiais->telefone}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700">{{$empresa_filiais->telefone}}</p>
                     </div>
                     <div>
                         <p>Email:</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400 break-all ">{{$empresa_filiais->email}} </p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700 break-all ">{{$empresa_filiais->email}} </p>
                     </div>
                     <div>
                         <p>Empresa Matriz :</p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400 ">{{$empresa_filiais->empresa->nome_empresa}}</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-700 ">{{$empresa_filiais->empresa->nome_empresa}}</p>
                     </div>
                     <div></div>
 
